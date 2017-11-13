494be09ed033748d5c79d23362572ee58eec3371
Barry Els <barryels@gmail.com>
2017-11-13 09:00:42 +0200

    added a few test examples

---
+++ 3/Main.elm
@@ -0,0 +1,41 @@
+module Main exposing (..)
+
+import Html exposing (..)
+import Html.Attributes exposing (..)
+
+
+type alias Something =
+    { a : Bool
+    , age : Int
+    }
+
+
+type alias Dog =
+    { name : String
+    , age : Int
+    }
+
+
+spock : Dog
+spock =
+    { name = "Spock"
+    , age = 3
+    }
+
+
+renderDog : Dog -> String
+renderDog dog =
+    dog.name ++ ", " ++ (toString dog.age)
+
+
+main : Html msg
+main =
+    Html.div
+        [ Html.Attributes.style
+            [ ( "background-color", "#c0c" )
+            , ( "text-align", "center" )
+            , ( "padding", "20px" )
+            ]
+        ]
+        [ Html.text <| renderDog spock
+        ]
